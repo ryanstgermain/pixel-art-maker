f3ad5c637445e860aed19330b46ab500a5060db9
ryanstgermain <ryanjoel26@gmail.com>
2018-09-20 14:54:55 -0600

    added rest of javascript

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var mainCanvas = document.querySelector('.canvas')
+var fillBox = document.getElementsByClassName('pixel')
+var pen = document.getElementById('red')
+var colorPick = document.getElementsByClassName('colorPicker')
 
 function createPixels(numberofpixels){
     for(var i = 0; i < numberofpixels; i++){
@@ -7,4 +10,18 @@ function createPixels(numberofpixels){
         mainCanvas.appendChild(newPixel)
     }
 }
-createPixels(6999) 
+createPixels(6999)
+
+for(var i = 0; i < colorPick.length; i++){
+    colorPick[i].addEventListener('click', function(event){
+    pen = event.target.id
+        console.log(pen)
+    })
+}
+
+for(var i = 0; i < fillBox.length; i++){
+   fillBox[i].addEventListener('click', function(event){
+   event.target.id = pen
+   }) 
+}
+
